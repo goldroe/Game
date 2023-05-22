2a4140fc0c81d24a04bdc8eca5d461b3bc047904
Carlos Rvera <cr4185320@gmail.com>
2023-05-21 22:37:32 -0500

    Added shader to draw outline border of quads.

--- src/win32_game.cpp
+++ src/win32_game.cpp
@@ -158,7 +158,81 @@ int main(int argc, char **argv) {
     Texture tile_sheet = GL_load_texture("data/jungle.png");
     Texture player_sheet = GL_load_texture("data/player.png");
 
-    float vertices[] = {
+
+    float rect_vertices[] = {
+        // position
+        0.0f, 0.0f,
+        0.0f, 1.0f,
+        1.0f, 1.0f,
+
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        1.0f, 0.0f,
+    };
+
+    unsigned int rect_vao;
+    glGenVertexArrays(1, &rect_vao);
+    glBindVertexArray(rect_vao);
+
+    unsigned int rect_vbo;
+    glGenBuffers(1, &rect_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, rect_vbo);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(rect_vertices), rect_vertices, GL_DYNAMIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
+
+    const char *rect_vsource =
+        "#version 330 core\n"
+        "layout (location = 0) in vec2 a_pos;\n"
+        "uniform mat4 wvp;\n"
+        "void main() {\n"
+        "gl_Position = wvp * vec4(a_pos.x, a_pos.y, 0.0f, 1.0f);\n"
+        "}\0";
+
+    const char *rect_fsource =
+        "#version 330 core\n"
+        "out vec4 frag_color;\n"
+        "uniform vec3 our_color;\n"
+        "void main() {\n"
+        "frag_color = vec4(our_color.r, our_color.g, our_color.b, 1.0);\n"
+        "}\0";
+
+    int status = 0;
+
+    unsigned int rect_vshader;
+    rect_vshader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(rect_vshader, 1, &rect_vsource, NULL);
+    glCompileShader(rect_vshader);
+
+    glGetShaderiv(rect_vshader, GL_COMPILE_STATUS, &status);
+    if (!status) {
+        printf("Failed to compile vertex shader!\n");
+    }
+
+    unsigned int rect_fshader;
+    rect_fshader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(rect_fshader, 1, &rect_fsource, NULL);
+    glCompileShader(rect_fshader);
+
+    glGetShaderiv(rect_fshader, GL_COMPILE_STATUS, &status);
+    if (!status) {
+        printf("Failed to compile vertex shader!\n");
+    }
+
+    unsigned int rect_program;
+    rect_program = glCreateProgram();
+    glAttachShader(rect_program, rect_vshader);
+    glAttachShader(rect_program, rect_fshader);
+    glLinkProgram(rect_program);
+    glDeleteShader(rect_vshader);
+    glDeleteShader(rect_fshader);
+    glUseProgram(rect_program);
+
+
+
+    float quad_vertices[] = {
         // position     texcoord
         0.0f, 0.0f,     0.0f, 0.0f,
         0.0f, 1.0f,     0.0f, 1.0f,
@@ -173,13 +247,20 @@ int main(int argc, char **argv) {
     glGenVertexArrays(1, &quad_vao);
     glBindVertexArray(quad_vao);
 
-    unsigned int vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    unsigned int quad_vbo;
+    glGenBuffers(1, &quad_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, quad_vbo);
 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quad_vertices), quad_vertices, GL_DYNAMIC_DRAW);
 
-    const char *vshader_source = "#version 330 core\n"
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
+
+
+    const char *quad_vsource =
+        "#version 330 core\n"
         "layout (location = 0) in vec2 a_pos;\n"
         "layout (location = 1) in vec2 a_tex_coord;\n"
         "out vec2 tex_coord;\n"
@@ -189,12 +270,8 @@ int main(int argc, char **argv) {
         "tex_coord = a_tex_coord;\n"
         "}\0";
 
-    unsigned int vshader;
-    vshader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vshader, 1, &vshader_source, NULL);
-    glCompileShader(vshader);
-
-    const char *fshader_source = "#version 330 core\n"
+    const char *quad_fsource = 
+        "#version 330 core\n"
         "out vec4 frag_color;\n"
         "in vec2 tex_coord;\n"
         "uniform sampler2D our_texture;\n"
@@ -206,24 +283,24 @@ int main(int argc, char **argv) {
         "frag_color = tex_color;\n"
         "}\0";
 
-    unsigned int fshader;
-    fshader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fshader, 1, &fshader_source, NULL);
-    glCompileShader(fshader);
+    unsigned int quad_vshader;
+    quad_vshader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(quad_vshader, 1, &quad_vsource, NULL);
+    glCompileShader(quad_vshader);
 
-    unsigned int shader_program;
-    shader_program = glCreateProgram();
-    glAttachShader(shader_program, vshader);
-    glAttachShader(shader_program, fshader);
-    glLinkProgram(shader_program);
-    glDeleteShader(vshader);
-    glDeleteShader(fshader);
-    glUseProgram(shader_program);
+    unsigned int quad_fshader;
+    quad_fshader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(quad_fshader, 1, &quad_fsource, NULL);
+    glCompileShader(quad_fshader);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+    unsigned int quad_program;
+    quad_program = glCreateProgram();
+    glAttachShader(quad_program, quad_vshader);
+    glAttachShader(quad_program, quad_fshader);
+    glLinkProgram(quad_program);
+    glDeleteShader(quad_vshader);
+    glDeleteShader(quad_fshader);
+    glUseProgram(quad_program);
 
     Game_Input game_input{};
     Game_State game_state{};
@@ -248,9 +325,8 @@ int main(int argc, char **argv) {
         glClearColor(0.08f, 0.08f, 0.08f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shader_program);
+        glUseProgram(quad_program);
         glBindVertexArray(quad_vao);
-
 
         HMM_Vec2 player_size = HMM_V2(100.0f, 100.0f);
         HMM_Vec2 tile_size = HMM_V2(300.0f, 100.0f);
@@ -270,14 +346,15 @@ int main(int argc, char **argv) {
 
         HMM_Vec4 atlas_trans = HMM_V4(0.0f / tile_sheet.width, 176.0f / (float)tile_sheet.height, 48.0f/tile_sheet.width, 16.0f / tile_sheet.height);
 
-        unsigned int wvp_loc = glGetUniformLocation(shader_program, "wvp");
-        unsigned int atlas_loc = glGetUniformLocation(shader_program, "atlas_trans");
+        unsigned int wvp_loc = glGetUniformLocation(quad_program, "wvp");
+        unsigned int atlas_loc = glGetUniformLocation(quad_program, "atlas_trans");
 
         glUniformMatrix4fv(wvp_loc, 1, GL_FALSE, (f32 *)&wvp);
         glUniform4fv(atlas_loc, 1, (f32 *)&atlas_trans);
 
         glBindTexture(GL_TEXTURE_2D, tile_sheet.id);
 
+        
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
         // draw player
@@ -295,6 +372,42 @@ int main(int argc, char **argv) {
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
 
+        // draw rect
+        glUseProgram(rect_program);
+        glBindVertexArray(rect_vao);
+
+        trans = HMM_Translate(HMM_V3(0.0f, 0.0f, 0.0f));
+        scale = HMM_Scale(HMM_V3(100.0f, 5.0f, 1.0f));
+        world = trans * scale;
+        wvp = projection * world;
+
+        HMM_Vec3 rect_color = HMM_V3(1.0f, 1.0f, 0.0f);
+        unsigned int rect_wvp_loc = glGetUniformLocation(rect_program, "wvp");
+        unsigned int rect_color_loc = glGetUniformLocation(rect_program, "our_color");
+        glUniformMatrix4fv(rect_wvp_loc, 1, GL_FALSE, (f32 *)&wvp);
+        glUniform3fv(rect_color_loc, 1, (f32 *)&rect_color);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        trans = HMM_Translate(HMM_V3(0.0f, 100.0f, 0.0f));
+        scale = HMM_Scale(HMM_V3(100.0f, 5.0f, 1.0f));
+        world = trans * scale;
+        wvp = projection * world;
+        glUniformMatrix4fv(rect_wvp_loc, 1, GL_FALSE, (f32 *)&wvp);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        trans = HMM_Translate(HMM_V3(0.0f, 0.0f, 0.0f));
+        scale = HMM_Scale(HMM_V3(5.0f, 100.0f, 1.0f));
+        world = trans * scale;
+        wvp = projection * world;
+        glUniformMatrix4fv(rect_wvp_loc, 1, GL_FALSE, (f32 *)&wvp);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        trans = HMM_Translate(HMM_V3(100.0f - 5.0f, 0.0f, 0.0f));
+        scale = HMM_Scale(HMM_V3(5.0f, 100.0f, 1.0f));
+        world = trans * scale;
+        wvp = projection * world;
+        glUniformMatrix4fv(rect_wvp_loc, 1, GL_FALSE, (f32 *)&wvp);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
 
