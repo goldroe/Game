d5d1f2df2b23df8e85898a1a4a14779cfd441e0c
Carlos Rivera <cr4185320@gmail.com>
2023-05-23 00:39:21 -0500

    Ignore linker warning

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ SET INCLUDES=-I ..\ext -I ..\ext\glad\include -I ..\ext\GLFW\include
 SET SRC=..\src\win32_game.cpp ..\ext\glad\src\glad.c
 SET WARNING_FLAGS=-W4 -WX -wd4100 -wd4101 -wd4189 -wd4996 -wd4530 -wd4201 -wd4505 -wd4098 -wd4700
 SET COMPILER_FLAGS=-nologo -FC -MDd -Zi %WARNING_FLAGS% %INCLUDES% -Fe:Game.exe
-SET LINKER_FLAGS=-debug -SUBSYSTEM:CONSOLE -LIBPATH:ext\GLFW ..\ext\GLFW\glfw3.lib opengl32.lib user32.lib gdi32.lib winmm.lib shell32.lib
+SET LINKER_FLAGS=-debug -SUBSYSTEM:CONSOLE -IGNORE:4098 -LIBPATH:ext\GLFW ..\ext\GLFW\glfw3.lib opengl32.lib user32.lib gdi32.lib winmm.lib shell32.lib
 
 IF NOT EXIST .build MKDIR .build
 PUSHD .build
